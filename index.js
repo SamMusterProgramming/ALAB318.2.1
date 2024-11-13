71e4a287013e00d4b3536e545df03877b410486f
samcoeur <samcoeur2013@gmail.com>
2024-11-12 20:06:36 -0500

    getting started , setting up express and other dependencies

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require ('express')
+const app = express();
+
+const PORT = 8080; 
+
+app.get('/',(req,res)=> {
+  
+})
+
+app.listen(PORT , ()=> {
+    console.log(`listenning on port ${PORT}`)
+})
