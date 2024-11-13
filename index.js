8cd650719a8064f12cb8def441e78a8fd3097879
samcoeur <samcoeur2013@gmail.com>
2024-11-12 20:41:53 -0500

    added users router route

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require ('express')
 const app = express();
+const userRoutes = require("./routes/users");
+app.use(express.json())
 
 const PORT = 8080; 
 
-app.get('/',(req,res)=> {
+
+
+app.use("/user", userRoutes);
+
+app.get('/',(req,res) => {
   
 })
 
-app.listen(PORT , ()=> {
+app.listen(PORT , () => {
     console.log(`listenning on port ${PORT}`)
 })
---
+++ routes/users.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const router = express.Router();
+
+const users =[ {
+     id:123,
+    name: "Samir Haddadi",
+    email : "samcoeur2013@gmail.com"
+},
+{
+    id:124,
+    name: "Nesta Parchment",
+    email : "parchNesta@gmail.com"
+}]
+let randomId = 124;
+router.get('/',(req,res)=> {
+    res.status(200).json(users)
+})
+router.get('/:userId',(req, res) => {
+   const userId = parseInt(req.params.userId) 
+   const user =  users.find(({id}) => id === userId) ; 
+   if(!user) return res.status(400).json(`could't find user ` )
+   return res.status(200).json(user)
+  })
+
+  
+  
+
+module.exports = router;
